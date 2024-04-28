0c2c6b09714227a13cc7e23c552c080a37213fe9
farel07 <farrqeel@gmail.com>
2024-04-28 10:11:57 +0700

    feat(auth):add login system

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\AuthController;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +18,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/auth/login', [AuthController::class, 'index'])->middleware('guest');
+Route::post('/auth/login', [AuthController::class, 'authenticate'])->name('auth');
