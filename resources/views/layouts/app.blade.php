3821f6d9927ca3c96eaf07ecc39b39e0c9391a73
hafidzrdwn <hafidz10032005@gmail.com>
2024-04-28 12:26:06 +0700

    feat(home): add landing page(client)

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,8 +12,12 @@
   </head>
   <body>
   @include('partials.navbar')
-    <div class="container pt-5">
+    <div class="container container-md-fluid pt-5 px-0">
         @yield('content')
+    </div>
+
+    <div class="container-fluid pt-5 pb-3 mx-0 px-0" style="background:#00215E;">
+      @include('partials.footer')
     </div>
     <script src="{{ asset('jquery/jquery-3.7.1.js') }}"></script>
     <script src="{{ asset('js/bootstrap.bundle.js') }}"></script>
